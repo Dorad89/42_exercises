91a46bb35840713eefd1369bd4d5512697998af8
Dorad Ulqinaku <dulqinak@c2r7s3.tirana.42.school>
2026-01-30 23:25:41 +0100

    ft_strcmp,ft_strcspn implemented

--- Level_1/firstword/first_word.c
+++ Level_1/firstword/first_word.c
@@ -6,7 +6,7 @@
 /*   By: dulqinak <dulqinak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 22:36:43 by dulqinak          #+#    #+#             */
-/*   Updated: 2026/01/19 23:22:13 by dulqinak         ###   ########.fr       */
+/*   Updated: 2026/01/30 22:33:29 by dulqinak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,16 @@ int main(int argc, char **argv)
 {
     if (argc != 2)
     {
-        write(1, "\n", 1);
-        return 0;
-    }
-    int i = 0;
+        int i = 0;
 
-    while(argv[1][i] == 32 || argv[1][i] == 9)
-        i++;
+        while(argv[1][i] == 32 || argv[1][i] == 9)
+            i++;
 
-    while((argv[1][i] != 32 && argv[1][i] != 9) && argv[1][i] != '\0')
-    {
-        write(1, &argv[1][i], 1);
-        i++;
+        while((argv[1][i] != 32 && argv[1][i] != 9) && argv[1][i])
+        {
+            write(1, &argv[1][i], 1);
+            i++;
+        }
     }
     write(1, "\n", 1);
-    return 0;
 }

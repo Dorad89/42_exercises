0e8770f611486f54ea87bd7f4a79339813589d16
Dorad Ulqinaku <dulqinak@c1r2s1.tirana.42.school>
2026-02-04 00:13:07 +0100

    is power of 2, max implemented

---
+++ Level_2/max/max.c
@@ -0,0 +1,16 @@
+int		max(int* tab, unsigned int len)
+{
+    if(len > 0)
+    {    
+        int i = 0;
+        int max = tab[0];
+
+        while(i < len)
+        {
+            if(max < tab[i])
+                max = tab[i];
+            i++;
+        }
+        return max;
+    }
+}

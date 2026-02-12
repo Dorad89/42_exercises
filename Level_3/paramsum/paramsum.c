a087c36e8efcb9e3e73a9204952688085bc290d7
Dorad89 <doradulqinaku1989@gmail.com>
2026-02-12 22:09:46 +0100

    paramsum implemented

---
+++ Level_3/paramsum/paramsum.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+void putnbr(int n)
+{
+    if(n > 9)
+        putnbr(n / 10);
+    char c = (n % 10 + '0');
+    write(1, &c, 1);
+}
+
+int main(int argc, char **argv)
+{
+    (void)argv;
+    putnbr(argc - 1);
+    write(1, "\n", 1);
+    return(0);
+}
